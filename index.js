232bfa520de32af3cef225ac543a5d1ee784ae0c
eowsley <66394238+eowsley@users.noreply.github.com>
2020-10-28 16:58:57 -0700

    final commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// { declare object } [ declare array ]
+
+let product = {   
+    name: 'Gummy Bears',
+    inStock: true,
+    price: 1.99,
+    flavors: ['orange', 'coconut', 'raspberry', 'lemon']
+};
